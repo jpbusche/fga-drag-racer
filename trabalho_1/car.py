95e3530c6acaa186d0b933386a2e6e397c7ffbed
Matheus Oliveira <matheus_oliveira30@hotmail.com>
2016-09-15 11:09:31 -0300

    Moving Car - Initial State

--- trabalho_1/car.py
+++ trabalho_1/car.py
@@ -1,33 +1,50 @@
 class Car:
-    #Drag constant for a Corvette
-    C_DRAG = 0.4257
-
-    def __intit__(self, mass, max_hp, max_toque):
+    def __init__(self, actor, name, mass, x, y):
         self.mass = mass
+        self.name = name
+        self.actor = actor
+
+        self.x = x
+        self.y = y
+
         self.hp = 0
-        self.max_hp = max_hp
-        self.max_toque = max_torque
+        self.max_hp = 0
+        self.max_torque = 0
         self.velocity = 0
         self.rpm = 1000
-        self.engine_torque = 0;
+        self.engine_torque = 0
+        self.crankshaft_torque = 0
         self.acceleration = 0
         self.forces = 0
+        self.gear = 0
+
+    def find_max_torque(self):
+        #Uses an array of some torque curves and interpolation
+        pass
 
     #Engine's forces
-    def traction(self, gear_ratio, differential_ratio):
+    def traction(self, differential_ratio, throttle_position):
+        #self.max_torque = find_max_torque(self.rpm)
+        self.max_torque = 475.7
+
+        self.engine_torque = self.max_torque * throttle_position
+
         # In foot-pounds
         self.hp = self.engine_torque * self.rpm / 5252
+
+        gear_ratio= [0.0, 2.66, 1.78, 1.0, 0.74, 0.50]
+        self.crankshaft_torque = (gear_ratio[self.gear] * differential_ratio) * 0.7
 
         #FDrive = Torque at 'x' rpm * gear_ratio * differential_ratio
         # * transmission_efficiency * wheel_radius
         # Wheel = 13.4 inches
         # Transmission efficiency - It's a guess
-        f_drive = self.engine_torque * gear_ratio * differential_ratio * 0.7 * 0.34
-
-
-
+        return self.engine_torque * self.crankshaft_torque / 0.34
 
     def calculate_forces(self):
+        #Drag constant for a Corvette
+        C_DRAG = 0.4257
+
         #Calculates moving resistance (MR = FDrag + FRollingResistance)
         f_drag = -(self.velocity * C_DRAG)
 
@@ -36,7 +53,7 @@ class Car:
         f_rr = f_drag * 30
 
         #Calculates the car's traction based on its configuration
-        f_traction = traction()
+        f_traction = self.traction(3.42 ,1)
 
         #Update car's forces
         self.forces = f_traction + f_rr + f_drag
@@ -44,3 +61,30 @@ class Car:
     def calculate_velocity(self, dt):
         self.acceleration = self.forces / self.mass
         self.velocity = self.velocity + (dt * self.acceleration)
+
+    def update(self, dt):
+        self.calculate_forces()
+        self.calculate_velocity(dt)
+
+        self.y -= self.velocity
+
+        self.print_stats()
+
+    def draw(self):
+        self.actor.y = self.y
+        self.actor.x = self.x
+        self.actor.draw()
+
+    def print_stats(self):
+        print("----------------------------------")
+        print("Name: " + self.name)
+        print("X Position: " + str(self.x))
+        print("Y Position: " + str(self.y))
+        print("Velocity: " + str(self.velocity))
+        print("RPM: " + str(self.rpm))
+        print("Gear: " + str(self.gear))
+        print("Forces: " + str(self.forces))
+        print("HP: " + str(self.hp))
+        print("Max HP: " + str(self.max_hp))
+        print("Max torque: " + str(self.max_torque))
+        print("----------------------------------\n")
--- trabalho_1/main.py
+++ trabalho_1/main.py
@@ -1,4 +1,41 @@
- from phys import *
- from car import *
+# Workarround for importing in pygamezero
+import sys; sys.path.append('.')
 
- 
+from car import *
+
+class World:
+    WIDTH = 800
+    HEIGHT = 600
+
+    def __init__(self, objects=[]):
+        self.objects = list(objects)
+
+    def update(self, dt):
+        for obj in self.objects:
+            obj.update(dt)
+
+    def draw(self):
+        for obj in self.objects:
+            obj.draw()
+
+    def add(self, obj):
+        self.objects.append(obj)
+
+
+
+# Starting pygamezero simulation
+world = World()
+
+def update(dt):
+    screen.clear()
+    world.update(dt)
+
+def draw():
+    world.draw()
+
+car_1 = Car(Actor('notacar75px'), "Carro 1", 1000, 300, 500)
+
+#Puts car_1 in first gear - Make it possible to move
+car_1.gear = 1
+
+world.add(car_1)
