c51562b8d4f7f27f997de5ed319b42d9d99ac1c3
Matheus Oliveira <matheus_oliveira30@hotmail.com>
2016-09-19 15:48:03 -0300

    Added RPM and Gears controls, braking and overlay text
    Signed-off-by: Matheus Oliveira <matheus_oliveira30@hotmail.com>
    Signed-off-by: Joao Paulo Busche <joaopaulo.busche@gmail.com>

--- trabalho_1/car.py
+++ trabalho_1/car.py
@@ -1,7 +1,7 @@
 import math
 
 class Car:
-    def __init__(self, actor, name, mass, x, y):
+    def __init__(self, actor, name, mass, x, y, max_rpm):
         self.mass = mass
         self.name = name
         self.actor = actor
@@ -12,8 +12,11 @@ class Car:
         self.hp = 0
         self.max_hp = 0
         self.max_torque = 0
+        self.throttle_position = 0
+
         self.velocity = 0
         self.rpm = 1000
+        self.max_rpm = max_rpm
         self.engine_torque = 0
         self.crankshaft_torque = 0
         self.acceleration = 0
@@ -21,31 +24,37 @@ class Car:
         self.gear = 0
 
     def find_max_torque(self):
-        #Uses an array of some torque curves and interpolation
-        pass
+        #To-do: Uses an array of some torque curves and interpolation
+        torque_array = [[1000, 390], [2000, 440], [3000, 455], [4000, 470],
+        [4400, 475], [5000, 460], [5000, 390]]
 
-
-    def set_gear(self):
-        if(self.rpm > 6500 and self.gear == 6):
-            self.rpm = 6500
-        elif(self.rpm > 6500 and self.gear < 6):
-            self.gear = self.gear + 1
+        for line in range(0,7):
+            if self.rpm >= torque_array[line][0] or self.rpm < torque_array[line + 1][0]:
+                return torque_array[line][1]
 
 
     #Engine's forces
-    def traction(self, differential_ratio, throttle_position):
+    def traction(self, differential_ratio):
         wheel_radius = 0.33
         gear_ratio= [0.0, 2.66, 1.30, 1.78, 1.0, 0.74, 0.50]
-        wheel_rotation_rate = self.velocity / wheel_radius
-        self.rpm = wheel_rotation_rate * gear_ratio[self.gear] * differential_ratio * 60 / 2 * math.pi
-        self.set_gear()
-        #self.max_torque = find_max_torque(self.rpm)
-        self.max_torque = 475.7
 
-        self.engine_torque = self.max_torque * throttle_position
+        # Workarround for wheel rotation ratio (Diveded by 10)
+        wheel_rotation_rate = self.velocity / wheel_radius / 10
 
-        # In foot-pounds
-        self.hp = self.engine_torque * self.rpm / 5252
+        if self.rpm <= self.max_rpm and self.throttle_position > 0.05:
+            self.rpm = wheel_rotation_rate * gear_ratio[self.gear] * differential_ratio * 60 / 2 * math.pi
+        elif self.throttle_position <= 0.05 and self.velocity < 8/3.6:
+            self.rpm = 900
+        elif self.rpm >= self.max_rpm:
+            self.rpm = self.max_rpm - 500
+            return 0
+
+        self.max_torque = self.find_max_torque()
+
+        self.engine_torque = self.max_torque * self.throttle_position
+
+        # Foot-pounds to N.m
+        self.hp = (self.engine_torque * self.rpm / 5252) * 1.335
 
         self.crankshaft_torque = (gear_ratio[self.gear] * differential_ratio) * 0.7
 
@@ -67,14 +76,17 @@ class Car:
         f_rr = f_drag * 30
 
         #Calculates the car's traction based on its configuration
-        f_traction = self.traction(3.42 ,1)
+        f_traction = self.traction(3.42)
 
         #Update car's forces
         self.forces = f_traction + f_rr + f_drag
 
     def calculate_velocity(self, dt):
         self.acceleration = self.forces / self.mass
-        self.velocity = self.velocity + (dt * self.acceleration)
+
+        # Workarround for limit rpm
+        if self.rpm <= self.max_rpm:
+            self.velocity = self.velocity + (dt * self.acceleration)
 
     def update(self, dt):
         self.calculate_forces()
