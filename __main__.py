7cd0d7656fc686f9ad634417a09ac6b6c842b1ac
João Paulo Busche <joaopaulo.busche@gmail.com>
2016-12-08 08:00:41 -0200

    Starting package of game

--- __main__.py
+++
@@ -1,8 +0,0 @@
-import os
-
-# Para subir o jogo no PyPi (pip3)
-# No setup.py deve se lingar a essa função!
-def main():
-    path = os.path.dirname(__file__)
-    os.chdir(path)
-    os.system('pgzerun main.py')
